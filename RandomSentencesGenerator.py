d2c2cd2dc2a61961aaabc1e62e84b4d7047251f2
Angel Karasotyanov <maxaaa9@abv.bg>
2023-10-15 21:27:46 +0300

    Initial commit

---
+++ RandomSentencesGenerator.py
@@ -0,0 +1,45 @@
+import random
+
+
+def randomizer(word: list) -> str:
+    return random.choice(word)
+
+
+names = ['Angel', 'Ivan', 'Dimitar', 'Georgi', 'Nikolay',
+         'Petar', 'Todor', 'Boris', 'Simeon', 'Krum', 'Maria',
+         'Elena', 'Anna', 'Stefka', 'Ivanka', 'Desislava',
+         'Tsvetana', 'Milena', 'Miglena', 'Nadezhda']
+
+places = ["Sofia",
+          "Plovdiv",
+          "Varna",
+          "Burgas",
+          "Ruse",
+          "Stara Zagora",
+          "Pleven",
+          "Sliven",
+          "Dobrich",
+          "Shumen"]
+
+verbs = ["be", "have", "do", "say", "get", "make", "go", "know", "take", "see"]
+
+nouns = ["stones", "cake", "apple", "laptop", "bikes"]
+
+adverbs = ["slowly", "diligently", "warmly", "sadly", "rapidly"]
+
+details = ["live at home", "work in the office", "walk on the street",
+           "run in the park", "swim in the pool", "read at the library",
+           "eat at the restaurant", "sleep in the bedroom", "study at school",
+           "play in the garden"]
+
+print("Hello, this is your first random sentences:")
+while True:
+    random_name = randomizer(names)
+    random_places = randomizer(places)
+    random_verbs = randomizer(verbs)
+    random_nouns = randomizer(nouns)
+    random_adverbs = randomizer(adverbs)
+    random_details = randomizer(details)
+
+    print(f"{random_name} from {random_places} {random_adverbs} {random_verbs} {random_nouns}.")
+    input("Click [Enter] button to generate a new one.")
---
+++ main.py
@@ -0,0 +1,5 @@
+# Simple python program
+# This random sentence generator is just for fun!
+# These sentences can provide humor and be a cool \
+# way to surprise others by sharing a standout sentence \
+# on social media platforms and gathering your network's reaction.
